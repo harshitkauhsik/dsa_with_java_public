99e78bbc3f9849b06a4416f9a45517245ed36d5d
harshitkaushik <harshitkaushik8445@gmail.com>
2025-03-21 13:17:56 +0530

    i have created this new  file

--- dsa_with_java/src/SearchInfinite.java
+++ dsa_with_java/src/SearchInfinite.java
@@ -6,15 +6,16 @@ public class SearchInfinite {
         while(start<=end)
         {
             int mid=start+(end-start)/2;
-            if (val[mid] == target) {
+             if (val[mid] == target) {
                 return mid;
             }
-            if (val[mid] < target) {
+            else if (val[mid] < target) {
                 start = mid + 1;
-            } else {
+            }
+            else
+            {
                 end = mid - 1;
             }
-
         }
         return -1;
     }
@@ -22,17 +23,19 @@ public class SearchInfinite {
     {
         int start=0;
         int end=1;
-
-        if(target>arr[end])
-        {
-           end=end+(end-start+1)*2;
-        }
+       while(target>arr[end]) {
+           end = end*2;
+           if(arr[end]>target)
+           {
+               end=end-1;
+           }
+       }
         return end;
     }
 
     public static void main(String[] args) {
-        int[] arr={4,5,6,7,8,9,12};
-        System.out.println(SearchInfinite.findElement(arr,7));
+        int[] arr={4,5,6,7,8,9,12,20,34,56,78,90,124,234,345,456,567,678,789,980,990,999,1000,1002,1003,1005,1008,2356,4567,8977};
+        System.out.println(SearchInfinite.findElement(arr,1002));
     }
 
 }
