1d15423318b49475ada86ecc6198fa4b1b8171d5
harshitkaushik <harshitkaushik8445@gmail.com>
2025-04-05 22:13:19 +0530

    i have created this new  file

--- dsa_with_java/src/CyclicSort.java
+++ dsa_with_java/src/CyclicSort.java
@@ -1,29 +1,28 @@
 import java.util.Arrays;
+
 public class CyclicSort {
-    static void sort(int[] arr)
-    {   int len=arr.length;
-        int i=0;
-        while( i<len)
-        {   int correct=arr[i]-1;
-            if(correct!=i)
-            {
-                swap(arr,i,correct);
-            }
-            else {
+    static void sort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+
+            // check bounds and avoid infinite swap on duplicates
+            if (arr[i] >= 1 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
                 i++;
             }
-
         }
     }
-    static void swap(int[] arr,int index,int correct)
-    {
-        int temp=arr[index];
-        arr[index]=arr[correct];
-        arr[correct]=temp;
+
+    static void swap(int[] arr, int index, int correct) {
+        int temp = arr[index];
+        arr[index] = arr[correct];
+        arr[correct] = temp;
     }
 
     public static void main(String[] args) {
-        int[] arr={7,6,1,2,3,4,5};
+        int[] arr = {9, 7, 8, 8, 6, 5, 4, 3, 2, 1};
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
---
+++ dsa_with_java/src/MissingInCS.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+public class MissingInCS {
+    static void found(int[] nums)
+    {
+        int i=0;
+        int len=nums.length;
+        while(i<len)
+        {
+            int real=nums[i];
+            if(real!=i)
+            {
+                swap(nums,real,i);
+            }
+
+            i++;
+        }
+    }
+    static void swap(int[] nums,int real,int i)
+    {
+        int temp=nums[real];
+      nums[real]=nums[i];
+      nums[i]=temp;
+    }
+    static int search(int[] nums)
+    {   int len=nums.length;
+        int i=0;
+        found(nums);
+        while(i<len)
+        {
+            if (nums[i] != i)
+            {
+                return i;
+            }
+            i++;
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={};
+        System.out.println(search(arr));
+        System.out.println(Arrays.toString(arr));
+      //  System.out.println(arr.length);
+    }
+}
